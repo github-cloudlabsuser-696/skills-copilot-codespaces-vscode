1e7e3ec47370e4b0051f529ed92375ca18e768f7
github-cloudlabsuser-696 <167423986+github-cloudlabsuser-696@users.noreply.github.com>
2024-06-18 09:24:49 +0000

    Copilot third commit

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoracc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
